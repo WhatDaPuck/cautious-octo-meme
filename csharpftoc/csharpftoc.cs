81a0bcb7d3d293f26d26ca6c8360c8b21b14f2ff
WhatDaPuck <cbock1337@gmail.com>
2017-07-31 18:50:55 -0400

    x

--- csharpftoc/csharpftoc.cs
+++ csharpftoc/csharpftoc.cs
@@ -14,7 +14,8 @@ namespace csharpftoc
             function();
             watch.Stop();
             var elapsedmillisec = watch.ElapsedMilliseconds;
-            Console.WriteLine($"Time taken:  {elapsedmillisec / 1000} seconds {elapsedmillisec % 1000} milliseconds");
+            var elapsedticks = watch.ElapsedTicks;
+            Console.WriteLine($"Time taken:  {elapsedmillisec / 1000} seconds {elapsedmillisec % 1000} milliseconds, and the total amount of ticks was {elapsedticks}");
             Console.ReadKey();
 
         }
@@ -23,7 +24,7 @@ namespace csharpftoc
         {
             double fahr, celsius;
             int lower, upper, step;
-                                      /*Implementation could be
+                                      /*Implementaion could be
                                         better but i dont car*/
             lower = 0;
             upper = 1000000;
@@ -33,9 +34,10 @@ namespace csharpftoc
             while (fahr <= upper)
             {
                 celsius = (5.0 / 9.0) * (fahr - 32.0);
-                Console.WriteLine($"{fahr}  {celsius}");
+                Console.WriteLine($"{fahr}  {celsius}"); //comment this line out if you want
                 fahr = fahr + step;
             }
         }
     }
 }
+
